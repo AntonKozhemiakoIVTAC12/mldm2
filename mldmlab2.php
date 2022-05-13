626270e994828b5e2e55ad3975fb737b18749a0a
AntonKozhemiakoIVTAC12 <98730634+AntonKozhemiakoIVTAC12@users.noreply.github.com>
2022-05-13 14:17:40 +0400

    Add files via upload

---
+++ mldmlab2.php
@@ -0,0 +1,25 @@
+<DOCTYPE html>
+    <html lang = "en">
+    <head>
+        
+        <link rel = "stylesheet" href = "">
+        <script type = "text/javascript" src = "scripts/script2.js"></script>
+    </head>
+    <body>
+    
+    <form method="" action = "">
+        <table>
+            <tr>
+                <td>Ввод элементов</td>
+                <td><textarea id = "array" placeholder="Введите пары элементов"></textarea></td>
+            </tr>
+            <tr>
+                <td><input type = "button" value = "Получить отношение" onclick="getValue()"></td>
+                <td><input type = "button" value = "Анализ" onclick="analysis()"></td>
+            </tr>
+        </table>
+    </form>
+    <div id = "Value"></div> <br>
+    <div id = "Analysis"></div> <br>
+    </body>
+    </html>
